9a256dc02e41ca0bba48a6dafbb7a4d231ce3d86
simjunbo <simuelunbo@gmail.com>
2022-12-31 02:12:42 +0900

    feat: note database 이름 추가

--- data/src/main/java/com/simuelunbo/simplenoteapp/data/local/database/NoteDatabase.kt
+++ data/src/main/java/com/simuelunbo/simplenoteapp/data/local/database/NoteDatabase.kt
@@ -8,4 +8,8 @@ import com.simuelunbo.simplenoteapp.data.local.entity.NoteHistory
 @Database(entities = [NoteHistory::class], version = 1)
 internal abstract class NoteDatabase : RoomDatabase(){
     abstract val noteDao: NoteDao
+
+    companion object {
+        const val DATABASE_NAME = "notes_db"
+    }
 }
