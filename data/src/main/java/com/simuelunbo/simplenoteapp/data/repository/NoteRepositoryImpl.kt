0863dccf1a4defcba47c6d93aa4c48a8650fea23
simjunbo <simuelunbo@gmail.com>
2022-12-31 01:53:21 +0900

    refactor: NoteRepositoryImpl model 변환 이슈 수정

--- data/src/main/java/com/simuelunbo/simplenoteapp/data/repository/NoteRepositoryImpl.kt
+++ data/src/main/java/com/simuelunbo/simplenoteapp/data/repository/NoteRepositoryImpl.kt
@@ -1,26 +1,47 @@
 package com.simuelunbo.simplenoteapp.data.repository
 
 import com.simuelunbo.simplenoteapp.data.local.dao.NoteDao
+import com.simuelunbo.simplenoteapp.data.local.entity.NoteHistory
 import com.simuelunbo.simplenoteapp.domain.model.Note
 import com.simuelunbo.simplenoteapp.domain.repository.NoteRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 internal class NoteRepositoryImpl(
     private val dao: NoteDao
-    ) : NoteRepository {
+) : NoteRepository {
     override fun getNotes(): Flow<List<Note>> {
-        return dao.getNotes()
+        return dao.getNotes().map { notes ->
+            convertNotes(notes)
+        }
     }
 
     override suspend fun getNoteById(id: Int): Note? {
-        return dao.getNoteById(id)
+        return dao.getNoteById(id)?.convertNoteHistoryToNote()
     }
 
     override suspend fun insertNote(note: Note) {
-        dao.insertNote(note)
+        dao.insertNote(convertNoteHistory(note))
     }
 
     override suspend fun deleteNote(note: Note) {
-        dao.deleteNote(note)
+        dao.deleteNote(convertNoteHistory(note))
     }
+
+
+    private fun convertNotes(notes: List<NoteHistory>): List<Note> {
+        return notes.map { note ->
+            note.convertNoteHistoryToNote()
+        }
+    }
+
+    private fun convertNoteHistory(note: Note): NoteHistory =
+        NoteHistory(
+            title = note.title,
+            content = note.content,
+            timestamp = note.timestamp,
+            color = note.color,
+            id = note.id
+        )
+
 }
