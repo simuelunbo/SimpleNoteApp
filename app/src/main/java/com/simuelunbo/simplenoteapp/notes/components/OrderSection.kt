461327fa411e7b1c681ecda74af6ab73bec62a04
simjunbo <simuelunbo@gmail.com>
2023-01-04 09:33:40 +0900

    feat: notesScreen UI 작업

---
+++ app/src/main/java/com/simuelunbo/simplenoteapp/notes/components/OrderSection.kt
@@ -0,0 +1,73 @@
+package com.simuelunbo.simplenoteapp.notes.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.simuelunbo.simplenoteapp.domain.util.NoteOrder
+import com.simuelunbo.simplenoteapp.domain.util.OrderType
+import com.simuelunbo.simpletodoapp.R
+
+@Composable
+fun OrderSection(
+    modifier: Modifier = Modifier,
+    noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending),
+    onOrderChange: (NoteOrder) -> Unit
+) {
+    Column(
+        modifier = Modifier
+    ) {
+        Row(
+            modifier = Modifier
+        ) {
+            DefaultRadioButton(
+                text = stringResource(
+                    id = R.string.main_radio_button_title
+                ),
+                selected = noteOrder is NoteOrder.Title, 
+                onSelect = { onOrderChange(NoteOrder.Title(noteOrder.orderType)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = stringResource(
+                    id = R.string.main_radio_button_date
+                ),
+                selected = noteOrder is NoteOrder.Date,
+                onSelect = { onOrderChange(NoteOrder.Date(noteOrder.orderType)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = stringResource(
+                    id = R.string.main_radio_button_color
+                ),
+                selected = noteOrder is NoteOrder.Color,
+                onSelect = { onOrderChange(NoteOrder.Color(noteOrder.orderType)) }
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            DefaultRadioButton(
+                text = stringResource(
+                    id = R.string.main_radio_button_Ascending
+                ),
+                selected = noteOrder.orderType is OrderType.Ascending,
+                onSelect = {
+                    onOrderChange(noteOrder.copy(OrderType.Ascending))
+                }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = stringResource(
+                    id = R.string.main_radio_button_Descending
+                ),
+                selected = noteOrder.orderType is OrderType.Descending,
+                onSelect = {
+                    onOrderChange(noteOrder.copy(OrderType.Descending))
+                }
+            )
+        }
+    }
+}
